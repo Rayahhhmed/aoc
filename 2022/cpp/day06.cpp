0d3d1620b74622f7d17ff6a93b9c859a4e358ea5
Ray <raiyan.ahmed@gmail.com>
2022-12-06 16:49:55 +1100

    kinda trivial today

--- 2022/cpp/day06.cpp
+++ 2022/cpp/day06.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 const string INPUT_STREAM_NAME = "day06.txt";
 
-void get_p1() {
+void get_p1(int &i) {
     string line;
     ifstream file(INPUT_STREAM_NAME);
     ios_base::sync_with_stdio(false);
@@ -14,30 +14,37 @@ void get_p1() {
     int sum = 0;
     if(file.is_open()) {
         while(getline(file, line)) {
-            
-        cout << "Sum for part 1: " << sum << '\n';
+        int l = 0;
+        int r = 0;
+        int alph[26] = {};
+        fill(alph, alph + 26, -1);
+        for (;r - l + 1 <= i; r++) {
+            // cout << "alph r =>" << alph[line[r] - 'a'] << endl;
+            if (alph[line[r] - 'a'] != -1) {
+                while (alph[line[r] - 'a'] != -1) { 
+                    alph[line[l] - 'a'] -= 1;
+                    l++;
+                }
+            }
+            alph[line[r] - 'a'] += 1;
+            //cout << line[r] << " " << alph[line[r] - 'a'] << endl;
+        }
+        
+        cout << line.substr(l, r - l) << '\n';
+        cout << r << endl;
         file.close();
+        return r;
     }
     } else cout << "Unable to open file";
 }
 
-void get_p2() {    
-    string line;
-    ifstream file(INPUT_STREAM_NAME);
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    int sum = 0;
-    if(file.is_open()) {
-        while(getline(file, line)) {
-            
-        }
-        cout << "Sum for part 2: " << sum << '\n';
-        file.close();
-    } else cout << "Unable to open file";
-}
+
 int main() {
-    get_p1();
-    get_p2();
+    int p1 = 4;
+    int p2 = 14;
+    cout << "P1: " << '\n';
+    get_p1(p1);
+    cout << "P2: " << '\n';
+    get_p1(p2);
     return 0;
 }
